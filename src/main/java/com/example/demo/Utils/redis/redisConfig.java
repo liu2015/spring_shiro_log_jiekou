606280ee2661fc7b06dae440ee0b66e1ffe8b363
os <liufuling14@163.com>
2020-01-01 21:37:41 +0800

    加入redis 的功能

---
+++ src/main/java/com/example/demo/Utils/redis/redisConfig.java
@@ -0,0 +1,36 @@
+package com.example.demo.Utils.redis;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * @ProjectName: demo
+ * @Package: com.example.demo.Utils.redis
+ * @ClassName: redisConfig
+ * @Author: os
+ * @Description:
+ * @Date: 01/01/20 下午9:23
+ * @Version: 1.0
+ */
+@Configuration
+public class redisConfig {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Bean
+    public RedisTemplate redisTemplateInit(){
+        //设置序列化key的实例化对象
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+
+        //设置序列化value的实例化对象
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+
+        return redisTemplate;
+    }
+
+}
